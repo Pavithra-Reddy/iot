691f9d680a095b445f144631a3bafbd100bc5c39
Pavithra-Reddy <pavithram28@gmail.com>
2018-04-13 10:39:42 +0530

    Add files via upload

---
+++ basic_led.ino
@@ -0,0 +1,29 @@
+
+char data = 0;                
+void setup() 
+{
+  Serial.begin(9600);         
+  pinMode(13, OUTPUT);        
+}
+void loop()
+{
+  if(Serial.available() > 0) 
+  {
+    data = Serial.read();      
+    Serial.print(data);       
+    Serial.print("\n");       
+    if(data == 0)           
+      digitalWrite(13, HIGH); 
+    if(data == 1)       
+      digitalWrite(13, LOW);   
+	if(data == 2)            
+      digitalWrite(12, HIGH);  
+    if(data == 3)      
+      digitalWrite(12, LOW);   
+	if(data == 4)            
+      digitalWrite(8, HIGH);  
+    if(data == 5)      
+      digitalWrite(8, LOW);  
+  }                            
+ 
+}                 
